8f8163635b7b38c11063171250a2624c5fd6b6da
kojima rikuto <kojikoji7765@icloud.com>
2021-12-10 15:57:07 +0900

    MFのアニメーションの追加

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -21,17 +21,11 @@ import Osimhen from "../assets/Osimhen.JPG";
 import Petagna from "../assets/Petagna.JPG";
 import Politano from "../assets/Politano.JPG";
 import Mertens from "../assets/Mertens.JPG";
-import Zielinski from "../assets/Zielinski.JPG";
-import Elmas from "../assets/Elmas.JPG";
-import Fabian from "../assets/Fabian.JPG";
 import Rrahmani from "../assets/Rrahmani.JPG";
 import Koulybaly from "../assets/Koulibaly.JPG";
 import DiLorenzo from "../assets/DiLorenzo.JPG";
 import Meret from "../assets/Meret.JPG";
 import Ospina from "../assets/Ospina.JPG";
-import Demme from "../assets/Demme.JPG";
-import Anguissa from "../assets/Anguissa.JPG";
-import Lobotka from "../assets/Lobotka.JPG";
 import Mario from "../assets/Mario.JPG";
 import Malcuit from "../assets/Malcuit.JPG";
 import Ghoulam from "../assets/Ghoulam.JPG";
@@ -55,6 +49,7 @@ import { useInView } from "react-intersection-observer";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import Header from "./Header";
 import { PacmanLoader } from "react-spinners";
+import { PlayersMF } from "./molecules/PlayersMF";
 
 function Players() {
   const [loading, setLoading] = useState(false);
@@ -427,244 +422,7 @@ function Players() {
               </MotionGrid>
             </MotionBox>
 
-            <MotionBox>
-              <Flex>
-                <MotionHeading
-                  color="blue.300"
-                  fontWeight="light"
-                  fontSize="6xl"
-                  id="MF"
-                  mt={8}
-                  variants={animationBounce}
-                >
-                  MF
-                </MotionHeading>
-              </Flex>
-              <Box mb={8}>
-                <Divider w={52} color="blue.300" />
-              </Box>
-              <MotionGrid
-                templateColumns={{
-                  sm: "repeat(1, 1fr)",
-                  md: "repeat(3,1fr)",
-                  lg: "repeat(3,1fr)",
-                  xl: "repeat(3,1fr)",
-                  base: "repeat(1,1fr)",
-                }}
-                gap={6}
-              >
-                <MotionBox _hover={{ opacity: 0.9 }} variants={fadeInUp}>
-                  <Image w="100%" src={Zielinski} borderRadius="lg" />
-                  <Accordion allowToggle mt={5}>
-                    <AccordionItem>
-                      <h2>
-                        <AccordionButton>
-                          <Box
-                            flex="1"
-                            textAlign="left"
-                            fontWeight="bold"
-                            color="blue.300"
-                          >
-                            Piotr Zieliński 20
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                      <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </MotionBox>
-
-                <MotionBox
-                  mt={{
-                    sm: "10",
-                    md: "0",
-                    lg: "0",
-                    xl: "0",
-                    base: "10",
-                  }}
-                  _hover={{ opacity: 0.9 }}
-                  variants={fadeInUp}
-                >
-                  <Image w="100%" src={Elmas} borderRadius="lg" />
-                  <Accordion allowToggle mt={5}>
-                    <AccordionItem>
-                      <h2>
-                        <AccordionButton>
-                          <Box
-                            flex="1"
-                            textAlign="left"
-                            fontWeight="bold"
-                            color="blue.300"
-                          >
-                            Elif Elmas 7
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                      <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </MotionBox>
-
-                <Box
-                  mt={{
-                    sm: "10",
-                    md: "0",
-                    lg: "0",
-                    xl: "0",
-                    base: "10",
-                  }}
-                  _hover={{ opacity: 0.9 }}
-                >
-                  <Image w="100%" src={Fabian} borderRadius="lg" />
-                  <Accordion allowToggle mt={5}>
-                    <AccordionItem>
-                      <h2>
-                        <AccordionButton>
-                          <Box
-                            flex="1"
-                            textAlign="left"
-                            fontWeight="bold"
-                            color="blue.300"
-                          >
-                            Fabian Ruiz 8
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                      <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </Box>
-                <Box
-                  mt={{
-                    sm: "10",
-                    md: "0",
-                    lg: "0",
-                    xl: "0",
-                    base: "10",
-                  }}
-                  _hover={{ opacity: 0.9 }}
-                >
-                  <Image w="100%" src={Demme} borderRadius="lg" />
-                  <Accordion allowToggle mt={5}>
-                    <AccordionItem>
-                      <h2>
-                        <AccordionButton>
-                          <Box
-                            flex="1"
-                            textAlign="left"
-                            fontWeight="bold"
-                            color="blue.300"
-                          >
-                            Diego Demme 4
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                      <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </Box>
-                <Box
-                  mt={{
-                    sm: "10",
-                    md: "0",
-                    lg: "0",
-                    xl: "0",
-                    base: "10",
-                  }}
-                  _hover={{ opacity: 0.9 }}
-                >
-                  <Image w="100%" src={Anguissa} borderRadius="lg" />
-                  <Accordion allowToggle mt={5}>
-                    <AccordionItem>
-                      <h2>
-                        <AccordionButton>
-                          <Box
-                            flex="1"
-                            textAlign="left"
-                            fontWeight="bold"
-                            color="blue.300"
-                          >
-                            André-Frank Zambo Anguissa 99
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                      <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </Box>
-                <Box
-                  mt={{
-                    sm: "10",
-                    md: "0",
-                    lg: "0",
-                    xl: "0",
-                    base: "10",
-                  }}
-                  _hover={{ opacity: 0.9 }}
-                >
-                  <Image w="100%" src={Lobotka} borderRadius="lg" />
-                  <Accordion allowToggle mt={5}>
-                    <AccordionItem>
-                      <h2>
-                        <AccordionButton>
-                          <Box
-                            flex="1"
-                            textAlign="left"
-                            fontWeight="bold"
-                            color="blue.300"
-                          >
-                            Stanislav Lobotka 68
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                      <AccordionPanel pb={4}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat.
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </Box>
-              </MotionGrid>
-            </MotionBox>
+                  <PlayersMF/>
 
             <Flex>
               <Heading
---
+++ src/components/molecules/PlayersMF.jsx
@@ -0,0 +1,276 @@
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Divider, Flex, Image } from "@chakra-ui/react";
+import { memo, useEffect } from "react";
+import { animationBounce, animationContainer, fadeInUp, MotionBox, MotionGrid, MotionHeading } from "../../animations/variants";
+
+import Zielinski from "../../assets/Zielinski.JPG";
+import Elmas from "../../assets/Elmas.JPG";
+import Demme from "../../assets/Demme.JPG";
+import Fabian from "../../assets/Fabian.JPG";
+import Lobotka from "../../assets/Lobotka.JPG";
+import Anguissa from "../../assets/Anguissa.JPG";
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+export const PlayersMF = memo(() => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    threshold: [0.3],
+    triggerOnce: false,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  return(
+    <MotionBox   ref={ref}
+    initial="hidden"
+    animate={controls}
+    variants={animationContainer}>
+    <Flex>
+      <MotionHeading
+        color="blue.300"
+        fontWeight="light"
+        fontSize="6xl"
+        id="MF"
+        mt={8}
+        variants={animationBounce}
+      >
+        MF
+      </MotionHeading>
+    </Flex>
+    <Box mb={8}>
+      <Divider w={52} color="blue.300" />
+    </Box>
+    <MotionGrid
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(3,1fr)",
+        lg: "repeat(3,1fr)",
+        xl: "repeat(3,1fr)",
+        base: "repeat(1,1fr)",
+      }}
+      gap={6}
+    >
+      <MotionBox _hover={{ opacity: 0.9 }} variants={fadeInUp}>
+        <Image w="100%" src={Zielinski} borderRadius="lg" />
+        <Accordion allowToggle mt={5}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box
+                  flex="1"
+                  textAlign="left"
+                  fontWeight="bold"
+                  color="blue.300"
+                >
+                  Piotr Zieliński 20
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </MotionBox>
+
+      <MotionBox
+        mt={{
+          sm: "10",
+          md: "0",
+          lg: "0",
+          xl: "0",
+          base: "10",
+        }}
+        _hover={{ opacity: 0.9 }}
+        variants={fadeInUp}
+      >
+        <Image w="100%" src={Elmas} borderRadius="lg" />
+        <Accordion allowToggle mt={5}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box
+                  flex="1"
+                  textAlign="left"
+                  fontWeight="bold"
+                  color="blue.300"
+                >
+                  Elif Elmas 7
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </MotionBox>
+
+      <MotionBox
+        mt={{
+          sm: "10",
+          md: "0",
+          lg: "0",
+          xl: "0",
+          base: "10",
+        }}
+        _hover={{ opacity: 0.9 }}
+        variants={fadeInUp}
+      >
+        <Image w="100%" src={Fabian} borderRadius="lg" />
+        <Accordion allowToggle mt={5}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box
+                  flex="1"
+                  textAlign="left"
+                  fontWeight="bold"
+                  color="blue.300"
+                >
+                  Fabian Ruiz 8
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </MotionBox>
+      <MotionBox
+        mt={{
+          sm: "10",
+          md: "0",
+          lg: "0",
+          xl: "0",
+          base: "10",
+        }}
+        _hover={{ opacity: 0.9 }}
+        variants={fadeInUp}
+      >
+        <Image w="100%" src={Demme} borderRadius="lg" />
+        <Accordion allowToggle mt={5}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box
+                  flex="1"
+                  textAlign="left"
+                  fontWeight="bold"
+                  color="blue.300"
+                >
+                  Diego Demme 4
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </MotionBox>
+      <MotionBox
+        mt={{
+          sm: "10",
+          md: "0",
+          lg: "0",
+          xl: "0",
+          base: "10",
+        }}
+        _hover={{ opacity: 0.9 }}
+        variants={fadeInUp}
+      >
+        <Image w="100%" src={Anguissa} borderRadius="lg" />
+        <Accordion allowToggle mt={5}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box
+                  flex="1"
+                  textAlign="left"
+                  fontWeight="bold"
+                  color="blue.300"
+                >
+                  André-Frank Zambo Anguissa 99
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </MotionBox>
+      <MotionBox
+        mt={{
+          sm: "10",
+          md: "0",
+          lg: "0",
+          xl: "0",
+          base: "10",
+        }}
+        _hover={{ opacity: 0.9 }}
+        variants={fadeInUp}
+      >
+        <Image w="100%" src={Lobotka} borderRadius="lg" />
+        <Accordion allowToggle mt={5}>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box
+                  flex="1"
+                  textAlign="left"
+                  fontWeight="bold"
+                  color="blue.300"
+                >
+                  Stanislav Lobotka 68
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </MotionBox>
+    </MotionGrid>
+  </MotionBox>
+  )
+})
